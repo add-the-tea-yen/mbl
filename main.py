dafadfd92b7025608e5072bd96d607fac6c76dae
Adityan <adityan041414@gmail.com>
2025-07-21 16:09:06 +0530

    main.py clean up

--- main.py
+++ main.py
@@ -2,10 +2,3 @@ import numpy as np
 from genmulti import EDUM
 from plot import *
 
-#plotAreaVolume('./spins/half/L14/psi.csv')
-L = [x for x in range(7,16)]
-#for i in L:
-    #plotPhases(f'./spins/half/L{i}_phases.csv')
-
-#plotEchoTime('./spins/half/L14/phases.csv','./spins/half/L14/psi.csv',14)
-
